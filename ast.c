a779edd979bd77327f2fca08ad88f3244a09e422
Guilherme Martins <gui.amado29@gmail.com>
2018-11-03 18:09:04 +0000

    added constructors for command structures

--- ast.c
+++ ast.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h> // for malloc
 #include "ast.h" // AST header
+#include <string.h> //for strdup
 
 Expr* ast_integer(int v) {
   Expr* node = (Expr*) malloc(sizeof(Expr));
@@ -33,5 +34,53 @@ Expr_bool* ast_operation_bool
   node->kind = E_OPERATION_BOOL;
   node->attr.op.left = left;
   node->attr.op.right = right;
+  return node;
+}
+
+
+//command constructor functions
+Cmd* ast_atrib(char* string, Expr* expr){
+  Cmd* node = (Cmd*) malloc(sizeof(Cmd));
+  node->kind = E_ATRIB;
+  node->attr.atrib.left = strdup(string);
+  //cria nova string igual e devolve apontador
+  node->attr.atrib.right = expr;
+}
+
+Cmd* ast_if(Expr_bool* expr, Cmd* list){
+  Cmd* node = (Cmd*) malloc(sizeof(Cmd*));
+  node->kind = E_IF;
+  node->attr._if.expr = expr;
+  node->attr._if.list = list;
+  return node;
+}
+
+Cmd* ast_ifelse(Expr_bool* expr, Cmd* list1, Cmd* list2){
+  Cmd* node = (Cmd*) malloc(sizeof(Cmd*));
+  node->kind = E_IFELSE;
+  node->attr._ifelse.expr = expr;
+  node->attr._ifelse.list1 = list1;
+  node->attr._ifelse.list2 = list2;
+  return node;
+}
+
+Cmd* ast_while(Expr_bool* expr, Cmd* list){
+  Cmd* node = (Cmd*) malloc(sizeof(Cmd*));
+  node->attr._while.expr = expr;
+  node->attr._while.list = list;
+  return node;
+}
+
+Cmd* ast_scan(char* var){
+  Cmd* node = (Cmd*) malloc(sizeof(Cmd));
+  node->kind = E_SCANF;
+  node->attr._scanf.var = strdup(var);
+  return node;
+}
+
+Cmd* ast_printf(Expr* expr){
+  Cmd* node = (Cmd*) malloc(sizeof(Cmd));
+  node->kind = E_PRINTF;
+  node->attr._printf.expr = expr;
   return node;
 }
