4de12830edbd68a1ce05b2f0f85a1f64b573a44f
Guilherme Martins <gui.amado29@gmail.com>
2018-11-03 16:34:33 +0000

    added commands structure

--- ast.h
+++ ast.h
@@ -34,8 +34,52 @@ struct _Expr_bool {
   } attr;
 };
 
+struct _Cmd {
+  enum {
+    E_ATRIB,
+    E_IF,
+    E_IFELSE,
+    E_WHILE,
+    E_SCANF,
+    E_PRINTF,
+    E_LISTCMD
+  } kind;
+  union {
+    struct {
+      char* left;
+      struct _Expr* right;
+    } atrib;
+    struct {
+      struct _Expr_bool* expr;
+      struct _Cmd* list;
+    } _if;
+    struct {
+      struct _Expr_bool* expr;
+      struct _Cmd* list1;
+      struct _Cmd* list2;
+    } _ifelse;
+    struct {
+      struct _Expr_bool* expr;
+      struct _Cmd* list;
+    } _while;
+    struct {
+      char* var;
+    } _scanf;
+    struct {
+      struct _Expr* expr; //n tenho a certeza deste
+    } _printf;
+    struct {
+      struct _Cmd* head;
+      struct _Cmd* tail;
+    } list_cmd;
+  } attr;
+};
+
 typedef struct _Expr Expr; // Convenience typedef
 typedef struct _Expr_bool Expr_bool;
+typedef struct _Cmd Cmd;
+typedef struct _Cmd_list Cmd_list;
+
 // Constructor functions (see implementation in ast.c)
 Expr* ast_integer(int v);
 Expr* ast_operation(int operator, Expr* left, Expr* right);
